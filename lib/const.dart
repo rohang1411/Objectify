f64a1dd5de9403bd223142dfdb69a22f22e862da
Rohan Sharma <51371041+rohang1411@users.noreply.github.com>
2021-02-09 10:46:11 +0530

    Delete const.dart

--- lib/const.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-TextStyle boldStyle(BuildContext context) {
-  return Theme.of(context).textTheme.bodyText1.copyWith(
-      fontWeight: FontWeight.bold, fontSize: 20, fontFamily: 'Segoe UI');
-}
